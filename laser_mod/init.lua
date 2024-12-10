8907efb351ef80856c96a1bc62dcdbc8f361ddc5
abop16 <kramer.aiden.a@gmail.com>
2024-12-10 11:19:10 -0500

    init.lua

--- laser_mod/init.lua
+++ laser_mod/init.lua
@@ -1,83 +1,62 @@
--- Table to store laser colors
-local laser_colors = {
-    red = "#FF0000",
-    green = "#00FF00",
-    blue = "#0000FF",
-    yellow = "#FFFF00",
-    purple = "#800080",
-    orange = "#FFA500",
-    white = "#FFFFFF",
-    black = "#000000",
-}
-
+-- Laser Mod for Minetest
 -- Register the laser emitter node
 minetest.register_node("laser_mod:laser_emitter", {
     description = "Laser Emitter",
     tiles = {"laser_emitter.png"},
     groups = {cracky = 3, oddly_breakable_by_hand = 1},
-    paramtype2 = "facedir",  -- Allows the node to have a facing direction
+    paramtype2 = "facedir",
     on_construct = function(pos)
         local meta = minetest.get_meta(pos)
-        meta:set_string("laser_color", "red") -- Default laser color
+        meta:set_string("laser_color", "red")  -- Default color
         local timer = minetest.get_node_timer(pos)
-        timer:start(0.1) -- Trigger laser periodically
+        timer:start(0.1)
     end,
-    on_timer = function(pos)
+    on_timer = function(pos, elapsed)
+        -- Find direction the laser is facing
         local node = minetest.get_node(pos)
-        -- Get the direction the node is facing (param2 defines the direction)
         local dir = minetest.facedir_to_dir(node.param2)
-        local start_pos = vector.add(pos, dir)  -- Start position is the emitter's position
+        local next_pos = vector.add(pos, dir)
 
-        -- Get laser color from metadata
+        -- Get the color from the metadata
         local meta = minetest.get_meta(pos)
-        local color = meta:get_string("laser_color")
-        local laser_color_code = laser_colors[color] or "#FF0000"  -- Default red if not set
+        local laser_color = meta:get_string("laser_color") or "red"
 
-        local laser_pos = vector.new(start_pos)
-        while true do
-            local node_at = minetest.get_node_or_nil(laser_pos)
-            if not node_at or node_at.name == "laser_mod:laser_receiver" then
-                break  -- Stop the laser at the receiver or air
+        -- Emit the laser particle
+        minetest.add_particlespawner({
+            amount = 1,
+            time = 0.1,
+            minpos = vector.add(pos, vector.new(0, 0.5, 0)),
+            maxpos = vector.add(pos, vector.new(0, 0.5, 0)),
+            minvel = vector.new(0, 0, 0),
+            maxvel = vector.new(0, 0, 0),
+            minsize = 1,
+            maxsize = 1,
+            texture = "laser_particle.png",
+            glow = 14, -- Brightness
+            color = laser_color,
+        })
+
+        -- Check for collision with nodes and mine the block
+        local target_node = minetest.get_node(next_pos)
+
+        if target_node.name ~= "air" and target_node.name ~= "laser_mod:laser_receiver" then
+            -- If the node is not air or a laser receiver, mine it (drop the node as an item)
+            local drops = minetest.get_node_drops(target_node.name, "")
+            for _, drop in ipairs(drops) do
+                minetest.add_item(next_pos, drop)  -- Drop the mined item at the position
             end
-
-            if node_at.name ~= "air" then
-                -- Destroy the node the laser hits
-                minetest.remove_node(laser_pos)
-            end
-
-            -- Check for players in the beam
-            local players = minetest.get_objects_inside_radius(laser_pos, 0.5)
-            for _, player in ipairs(players) do
-                if player:is_player() then
-                    player:set_hp(player:get_hp() - 10)  -- Damage player
-                end
-            end
-
-            -- Add the particle effect to visualize the laser
-            minetest.add_particle({
-                pos = laser_pos,
-                velocity = {x = 0, y = 0, z = 0},
-                acceleration = {x = 0, y = 0, z = 0},
-                expirationtime = 0.1,
-                size = 4,
-                collisiondetection = false,
-                vertical = false,
-                texture = "laser_particle.png",  -- Laser particle texture
-            })
-
-            -- Move the laser beam forward one step
-            laser_pos = vector.add(laser_pos, dir)
+            -- Remove the node from the world
+            minetest.set_node(next_pos, {name = "air"})
         end
 
-        return true  -- Continue the timer
+        return true
     end,
-    on_punch = function(pos, _, puncher, pointed_thing)
-        local item = puncher:get_wielded_item()
-        local dye_name = item:get_name():match("^dye:(.+)$") -- Check if it's a dye
-        if dye_name and laser_colors[dye_name] then
-            local meta = minetest.get_meta(pos)
-            meta:set_string("laser_color", dye_name)
-            minetest.chat_send_player(puncher:get_player_name(), "Laser color changed to " .. dye_name)
+    on_punch = function(pos, node, puncher, pointed_thing)
+        -- When punched with a dye, change the laser color
+        local meta = minetest.get_meta(pos)
+        local color = puncher:get_wielded_item():get_name()
+        if string.sub(color, 1, 4) == "dye:" then
+            meta:set_string("laser_color", string.sub(color, 5))
         end
     end,
 })
@@ -88,25 +67,13 @@ minetest.register_node("laser_mod:laser_receiver", {
     tiles = {"laser_receiver.png"},
     groups = {cracky = 3, oddly_breakable_by_hand = 1},
     on_construct = function(pos)
-        -- Placeholder for any receiver-specific logic
+        -- Laser receiver does nothing special on construction
     end,
 })
 
--- Add crafting recipes (optional)
-minetest.register_craft({
-    output = "laser_mod:laser_emitter",
-    recipe = {
-        {"default:steel_ingot", "default:mese_crystal", "default:steel_ingot"},
-        {"", "default:glass", ""},
-        {"", "default:steel_ingot", ""},
-    },
+-- Register a simple item to represent the laser item drops
+minetest.register_craftitem("laser_mod:laser_item", {
+    description = "Laser Item",
+    inventory_image = "laser_item.png",
 })
 
-minetest.register_craft({
-    output = "laser_mod:laser_receiver",
-    recipe = {
-        {"default:steel_ingot", "default:mese_crystal", "default:steel_ingot"},
-        {"", "default:glass", ""},
-        {"", "default:steel_ingot", ""},
-    },
-})
